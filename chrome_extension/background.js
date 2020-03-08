de525c5b5f9f89c9c2c73f67ba4f622976ce7282
James Liang <james.liang.cje@icloud.com>
2020-03-08 18:21:03 -0400

    finished chrome extension with popup

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,1 +1,22 @@
 console.log("Chrome Extension iNews is running.")
+
+function onClickHandler(info,tab){
+  console.log("HELLO?")
+  console.log(tab)
+  let url = info.linkUrl
+  console.log(url,info)
+  let msg = {
+    url, info
+  }
+  chrome.tabs.sendMessage(tab.id,msg)
+}
+
+
+chrome.runtime.onInstalled.addListener(()=>{
+  let context = "link"
+  let title = "iNews"
+  let id = chrome.contextMenus.create({title,contexts : [context],id : `context-${context}`})
+})
+
+chrome.contextMenus.onClicked.addListener(onClickHandler)
+
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -2,7 +2,10 @@ let iNewsUrl = "localhost:5000/mediacheck"
 let backendUrl = "http://localhost:5000/"
 let aiBackendUrl = "http://localhost:5001/"
 const NOFETCH = "Failed to fetch"
-console.log("chrome extension go?")
+console.log("chrome extension go!")
+
+let INewsData = {}
+let webPageUrl = "http://localhost:3000"
 
 async function postRequest(completeUrl, data) {
   let resp = await fetch(completeUrl, {
@@ -35,48 +38,102 @@ async function urlExtract(url) {
   return json
 }
 
-async function verifyUrl(e){
-  let element = e.currentTarget
-  if (element.dataset.checkedByINews){
-    return
-  }
-  element.dataset.checkedByINews = true
-  let url = element.attributes["href"].value
+function closePopup() {
+  let popupElem = document.querySelector(".inews-popup")
+  popupElem.parentElement.removeChild(popupElem)
+}
+
+function preparePopup({bottom,left}){
+  let bodyRect = document.body.getBoundingClientRect()
+
+  let INewsPopup = document.createElement("div")
+  INewsPopup.style.position = "absolute"
+  INewsPopup.style.top = bottom - bodyRect.y + "px"
+  INewsPopup.style.left = left - bodyRect.x + "px"
+  INewsPopup.classList.add("inews-popup")
+  INewsData.popup = INewsPopup
+  document.body.appendChild(INewsPopup)
+}
+
+function showLoader(){
+  const INewsPopup = INewsData.popup
+  INewsPopup.innerHTML = `
+    <div class = "inews-popup-title"> Analyzing source ... </div>
+  `
+}
+
+function showHasData({credibility,category,url,name},newsUrl){
+  const INewsPopup = INewsData.popup
+  let popupTitle = credibility ? "The source seems credible" : "The source is not credible"
+  let encodedNewsUrl = encodeURIComponent(newsUrl)
+  INewsPopup.innerHTML = `
+    <div class = "inews-popup-title"> ${popupTitle} </div>
+    <div class = "inews-popup-source">${name} is classified as ${category}.<br/> <a href = "${url}" class="inews-popup-link">Learn more</a> </div>
+    <a class = "inews-popup-details" href="${webPageUrl}/dashboard/${encodedNewsUrl}">Perform a deeper analysis</a>
+    <div class = "inews-popup-xmark">&times;</div>
+  `
+
+}
+function showNoData(newsUrl){
+  const INewsPopup = INewsData.popup
+  let encodedNewsUrl = encodeURIComponent(newsUrl)
+  INewsPopup.innerHTML = `
+    <div class = "inews-popup-title"> Source not found  </div>
+    <a class = "inews-popup-details" href="${webPageUrl}/dashboard/${encodedNewsUrl}">Perform a deeper analysis</a>
+    <div class = "inews-popup-xmark">&times;</div>
+  `
+}
+async function createToolTip(boundingRect,url) {
+  //i want to make a popup such that the upper left corner of the popup is at the bottom left corner, so basically its kinda like a rectangle directly underneath the link
+  preparePopup(boundingRect)
+  showLoader()
+  let data = await mediaCheck(url)  
+  console.log(data)
   
-  if (!url.startsWith("http") && !url.startsWith("www")){
-    return
-  }
-  console.log(element)
-  console.log(url)
-  if (url === undefined){
-    return
-  } 
-  let json = await mediaCheck(url)
-  console.log(json)
-  const {credibility, category, mbfcUrl,name, hasData} = json
-  if (!hasData){
-    src = "images/unknown.png"
-  } else if (credibility){
-    src = "images/credible.png"
-    
+  if (data.hasData){
+    showHasData(data,url)
   } else {
-    src = "images/notcredible.png"
+    showNoData()
   }
-  let completeSrc = chrome.extension.getURL(src)
-  let categoryNode = hasData ? `[${category}]` : ''
-  element.innerHTML = `<img src = "${completeSrc}" style = "height:1em;"/>` + categoryNode + element.innerHTML
+  let elem = document.querySelector(".inews-popup-xmark")
+  console.log(elem)
+  elem.addEventListener("click",()=>{
+    closePopup()
+  })
+
+}
+
+chrome.runtime.onMessage.addListener(gotMessage)
+function gotMessage(message, sender, sendResponse) {
+  console.log({ message, sender, sendResponse })
+  const element = INewsData.selected
+  console.log("retrieved element", element)
+  let boundingRect = element.getBoundingClientRect()
+  const {url} = message
+  createToolTip(boundingRect,url)
+}
+
+function loadStyleSheet() {
+  let cssUrl = chrome.extension.getURL("style.css")
+  let link = document.createElement("link")
+  link.rel = "stylesheet"
+  link.type = "text/css"
+  link.href = cssUrl
+  link.onload = "alert('loaded stylesheet')"
+  document.head.appendChild(link)
 }
 
 
-function setup() {
-  let shouldContinue = confirm("continue using iNews")
-  if (!shouldContinue){
-    return
-  }
-  for (let aTag of document.querySelectorAll("a")) {
-    aTag.addEventListener("mouseover",verifyUrl)
-  }
+window.oncontextmenu = (e, b) => {
+  console.log(e)
+  console.log(b)
+  let element = e.target
+  console.log("selected element", element)
+  INewsData.selected = element
 }
 
 
-document.body.onload = setup
+document.body.onload = function () {
+  loadStyleSheet()
+  console.log("Load style sheet")
+}
